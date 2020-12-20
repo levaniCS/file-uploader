251a76fed6a93d3f5c9000485cd7578264d548ca
levaniCS <levanisarishvili3105@gmail.com>
2020-12-20 17:10:47 +0400

    front logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const fileRef = useRef(null)
+  const [loading, setLoading] = useState(false)
+ 
+
+  const onFileUpload = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    if(!fileRef.current.files[0]) {
+      setLoading(false)
+      return
+    }
+    const formData = new FormData(); 
+   
+    formData.append( 
+      "myFile", 
+      fileRef.current.files[0], 
+      fileRef.current.files[0].name
+    ); 
+   
+    
+    // Upload File to project directory
+
+    // Set default state
+    fileRef.current.value = null 
+    setLoading(false)
+  }; 
+
+
+  if(loading) {
+    return <div className="App">Loading...</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>File Uploader - React</h1>
+     <hr />
+      <form onSubmit={onFileUpload}> 
+        <input 
+          type="file" 
+          accept=".jpg, .jpeg, .png, .gif, .tiff"
+          ref={fileRef}
+        /> 
+        <button> Upload! </button> 
+      </form> 
     </div>
   );
 }
